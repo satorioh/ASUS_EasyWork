2e4e804a6b39fe947752e990e4541b6d6708f5b5
Roubin <wangbinxp@sina.com>
2017-09-03 22:10:27 +0800

    baidu map change code

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -30,8 +30,8 @@ export class ContactPageComponent implements OnInit{
   loadMap() {
     //初始化地图
     var map = new BMap.Map(this.mapElement.nativeElement);
-     var point = new BMap.Point(120.61990712, 31.31798737);
-     map.centerAndZoom(point, 15);
+      var point = new BMap.Point(120.61990712, 31.31798737);
+      map.centerAndZoom(point, 15);
     //加载地图插件
     map.addControl(new BMap.NavigationControl());
     map.addControl(new BMap.GeolocationControl());
@@ -46,9 +46,9 @@ export class ContactPageComponent implements OnInit{
         var translateCallback = function (data){
           if(data.status === 0) {
             console.log(data.points[0]);
-            var point = new BMap.Point(data.points[0]);
+            //var fixedPoint = new BMap.Point(data.points[0]);
             //console.log(point);
-            map.centerAndZoom(point, 15);
+            map.centerAndZoom(data.points[0], 15);
             var mk = new BMap.Marker(data.points[0]);
             map.addOverlay(mk);
             map.panTo(data.points[0]);
@@ -57,6 +57,14 @@ export class ContactPageComponent implements OnInit{
             //var label = new BMap.Label("转换后的百度坐标（正确）",{offset:new BMap.Size(20,-10)});
             //marker.setLabel(label); //添加百度label
             //bm.setCenter(data.points[0]);
+            //console.log('您的位置：'+r.point.lng+','+r.point.lat);
+            var geoc = new BMap.Geocoder();
+            geoc.getLocation(data.points[0], function(rs){
+              var addComp = rs.addressComponents;
+              var posMessage = "您当前的位置："+ addComp.province + ", " + addComp.city + ", " + addComp.district;
+              document.getElementById('position').textContent = posMessage;
+              console.log(posMessage);
+            });
           }
         };
     //坐标转换
@@ -68,14 +76,7 @@ export class ContactPageComponent implements OnInit{
         }, 1000);
 
 
-        //console.log('您的位置：'+r.point.lng+','+r.point.lat);
-        var geoc = new BMap.Geocoder();
-        geoc.getLocation(point, function(rs){
-          var addComp = rs.addressComponents;
-          var posMessage = "您当前的位置："+ addComp.province + ", " + addComp.city + ", " + addComp.district;
-          document.getElementById('position').textContent = posMessage;
-          console.log(posMessage);
-        });
+
         }else {
         document.getElementById('position').textContent = "您当前的位置：定位失败";
         console.log('failed'+this.getStatus());
